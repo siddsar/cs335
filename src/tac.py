7a7414a3f8a6691776752ffad9355cb6bf95093c
Siddhant Sarkar <siddhantsarkar2404@gmail.com>
2019-03-13 16:55:51 +0530

    tac.py added

---
+++ src/tac.py
@@ -0,0 +1,10 @@
+class TAC:
+
+    def __init__():
+        self.code = []
+
+    def emit(list_to_append):
+        code.append(list_to_append)
+
+    def print_tac():
+        for ln in range(1, len(code) + 1)

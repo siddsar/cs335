c347d4724f3488f5e2f66cce4073a6c1c2b0d49c
Siddhant Sarkar <siddhantsarkar2404@gmail.com>
2019-04-15 13:03:09 +0530

    code gen =

--- src/tac.py
+++ src/tac.py
@@ -159,68 +159,63 @@ class TAC:
 						val = int(item[1]) or int(item[2])
 					else:
 						val = int(item[1])^int(item[2])
-					print("mov $" + str(val) + ",-" + str(res_var['offset'])+"(%ebp)")
+					print("\tmov $" + str(val) + ",-" + str(res_var['offset'])+"(%ebp)")
 
 				else:
 
 					op = self.ST.find(item[2])
-					print("mov $"+ item[1] +",%ebx")
-					print("mov -"+str(op['offset'])+"(%ebp),%eax")
+					print("\tmov $"+ item[1] +",%ebx")
+					print("\tmov -"+str(op['offset'])+"(%ebp),%eax")
 
 					if item[3]=='&':
-						print("and %ebx,%eax")
+						print("\tand %ebx,%eax")
 					elif item[3]=="||":
-						print("or %ebx,%eax")
+						print("\tor %ebx,%eax")
 					else:
-						print("xor %ebx,%eax")
+						print("\txor %ebx,%eax")
 
-					print("mov %ebx, -"+str(res_var['offset'])+"(%ebp)")
+					print("\tmov %ebx, -"+str(res_var['offset'])+"(%ebp)")
 					
 			else:
 
 				if self.ST.find(item[2])==None:
 					op = self.ST.find(item[1])
-					print("mov $"+ item[2] +",%ebx")
-					print("mov -"+str(op['offset'])+"(%ebp),%eax")
+					print("\tmov $"+ item[2] +",%ebx")
+					print("\tmov -"+str(op['offset'])+"(%ebp),%eax")
 
 					if item[3]=='&':
-						print("and %ebx,%eax")
+						print("\tand %ebx,%eax")
 					elif item[3]=="||":
-						print("or %ebx,%eax")
+						print("\tor %ebx,%eax")
 					else:
-						print("xor %ebx,%eax")
+						print("\txor %ebx,%eax")
 
-					print("mov %ebx, -"+str(res_var['offset'])+"(%ebp)")
+					print("\tmov %ebx, -"+str(res_var['offset'])+"(%ebp)")
 					
 				else:
 					op1 = self.ST.find(item[1])
 					op2 = self.ST.find(item[2])
-					print("mov -"+str(op1['offset'])+"(%ebp),%ebx")
-					print("mov -"+str(op2['offset'])+"(%ebp),%eax")
+					print("\tmov -"+str(op1['offset'])+"(%ebp),%ebx")
+					print("\tmov -"+str(op2['offset'])+"(%ebp),%eax")
 
 					if item[3]=='&':
-						print("and %ebx,%eax")
+						print("\tand %ebx,%eax")
 					elif item[3]=="||":
-						print("or %ebx,%eax")
+						print("\tor %ebx,%eax")
 					else:
-						print("xor %ebx,%eax")
+						print("\txor %ebx,%eax")
 
-					print("mov %ebx, -"+str(res_var['offset'])+"(%ebp)")
+					print("\tmov %ebx, -"+str(res_var['offset'])+"(%ebp)")
 					
-		# elif item[3]=='=':
-			# res_var = self.ST.find(item[0])
-			# op = self.ST.find(item[1])
-			# if op==None:
-			# 	print("mov $"+str(res_var['offset'])+",%eax")
-			# 	print(" addl %rbp,%eax")
-			# 	print("mov $"+str(item[1])+",0(%eax)")
-			# else:
-			# 	print("mov $"+str(op['offset'])+",%ebx")
-			# 	print("addl %rbp,%ebx")
-			# 	print("mov (%ebx),%eax")
-			# 	print("mov $"+str(res_var['offset'])+",%ebx")
-			# 	print("addl %rbp,%ebx")
-			# 	print("mov %eax,0(%ebx)")
+		elif item[3]=='=':
+			res_var = self.ST.find(item[0])
+			op = self.ST.find(item[1])
+			if op==None:
+				print("\tmov $"+item[1]+",%eax")
+			else:
+				print("\tmov -"+str(op['offset'])+"%(ebp),%eax")
+			print("\t mov %eax, -" + str(res_var['offset']) + "(%ebp)")
+			
 
     def emit(self,list_to_append):
         self.code.append(list_to_append)

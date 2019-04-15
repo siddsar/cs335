9bfca51af5d6ac936187e11beab6e344ab9aa675
Akarsh Goyal <akarshg14@gmail.com>
2019-04-15 16:03:03 +0530

    no algb codegen

--- tests/simple.java
+++ tests/simple.java
@@ -1,5 +1,18 @@
 class Debug {
+    // int add(int a, int b) {
+    //     return a + b;
+    // }
     int main() {
-        int a = 4;
+        // int a = 19;
+        // int b = 2;
+        // int d = 1 ;
+        // boolean c;
+        // c = a && b || d;
+        // c = ~a;
+        int n=3;	
+        for(int i = 0;i < n;i=i+1){
+    		int x=2;
+    	}
     }
+
 }

2726e684d335076896cf416268cdbcf527621d6b
Akarsh Goyal <akarshg14@gmail.com>
2019-04-16 08:11:22 +0530

    dge

--- src/a.s
+++ src/a.s
@@ -6,22 +6,9 @@
 	inFormat:
 	.string "%d\n"
 	.global main
-main:
+ifib:
 	push %ebp
 	mov %esp, %ebp
 	add $-512, %esp
-	mov $42, %eax
+	mov 8(%ebp), %eax
 	mov %eax, -4(%ebp)
-	push -4(%ebp)
-	push $outFormatInt
-	call printf
-	add $8, %esp
-	mov -4(%ebp),%ebx
-	add $5,%ebx
-	mov %ebx, -8(%ebp)
-	push -8(%ebp)
-	push $outFormatInt
-	call printf
-	add $8, %esp
-	mov $1, %eax
-	int $0x80
